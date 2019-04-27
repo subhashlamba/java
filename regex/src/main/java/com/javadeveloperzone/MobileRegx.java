7d1356f5bda1cfd942506dd7afefb2aac0c63450
subhashlamba <subhashlammba@gmail.com>
2019-04-27 11:59:33 +0530

    added regular expression for mobile number

---
+++ regex/src/main/java/com/javadeveloperzone/MobileRegx.java
@@ -0,0 +1,28 @@
+package main.java.com.javadeveloperzone;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileRegx {
+
+    public static final Pattern MOBILE_REGX = Pattern.compile("^(\\+?\\d{2})?(\\s+)?(\\d{10})\\b");//. represents single character
+
+    public static void main(String[] args){
+
+        System.out.println(isValidMobileNo("0123456789"));                  // true
+        System.out.println(isValidMobileNo("+91 0123456789"));              // true
+        System.out.println(isValidMobileNo("91 0123456789"));              // true
+        System.out.println(isValidMobileNo("+910123456789"));               // true
+        System.out.println(isValidMobileNo("+91          0123456789"));     // true
+
+        System.out.println(isValidMobileNo("0123456789000"));                  // false
+        System.out.println(isValidMobileNo("0123 456 789"));                  // false
+
+    }
+    public static boolean isValidMobileNo(String mobileNo){
+        Matcher matcher = MOBILE_REGX.matcher(mobileNo);
+        return matcher.matches();
+
+    }
+}
+
